ac0c0e2f16211b84dfcc351ad28cff8a9c10600c
Chris Simoni <csimoni@kwikom.com>
2024-01-02 16:11:02 -0600

    copy code button changes

--- templates/base_templ.go
+++ templates/base_templ.go
@@ -89,7 +89,7 @@ func Base() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</script><script type=\"application/javascript\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</script><script type=\"text/javascript\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
