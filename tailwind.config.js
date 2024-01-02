b345d02e85acc753cf4e741eda6179ff56788c5e
Chris Simoni <csimoni@kwikom.com>
2024-01-02 10:53:41 -0600

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./templates/**/*.{html,js}",
+        "./templates/**/*.{html,js,templ}",
     ],
     theme: {
         extend: {
